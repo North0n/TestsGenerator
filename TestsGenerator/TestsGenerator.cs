b5bec20ae41947ca72970f0de02f57c0eec5314f
North0n <mautin.ivan.3@gmail.com>
2022-11-03 22:51:49 +0300

    Add generation of classes wrap

--- TestsGenerator/TestsGenerator.cs
+++ TestsGenerator/TestsGenerator.cs
@@ -1,5 +1,53 @@
-﻿namespace TestsGenerator;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
-public class TestsGenerator
+namespace TestsGenerator
 {
+    public class TestsGenerator
+    {
+        public List<string> Generate(string source)
+        {
+            var root = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+            
+            var usings = root.DescendantNodes().OfType<UsingDirectiveSyntax>();
+            var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+
+            var classesDeclarations = classes.Select(classDeclaration => GenerateTestsClass(classDeclaration))
+                .ToList();
+            
+            return classesDeclarations.Select(decl =>
+                CompilationUnit().WithUsings(new SyntaxList<UsingDirectiveSyntax>(usings)).AddMembers(decl)
+                    .NormalizeWhitespace().ToFullString()).ToList();
+        }
+
+        private MemberDeclarationSyntax GenerateTestsClass(ClassDeclarationSyntax classDeclaration)
+        {
+            SyntaxNode? current = classDeclaration;
+            var testClassDeclaration = ClassDeclaration(classDeclaration.Identifier.Text + "Tests")
+                .AddModifiers(Token(SyntaxKind.PublicKeyword));
+            var isFirstNamespace = true;
+            var currentTree = current;
+            while (current.Parent is NamespaceDeclarationSyntax parent)
+            {
+                NamespaceDeclarationSyntax ns;
+                if (isFirstNamespace)
+                {
+                    ns = NamespaceDeclaration(IdentifierName(parent.Name + ".Tests"))
+                        .WithMembers(new SyntaxList<MemberDeclarationSyntax>(testClassDeclaration));
+                    isFirstNamespace = false;
+                }
+                else
+                {
+                    ns = NamespaceDeclaration(parent.Name)
+                        .WithMembers(new SyntaxList<MemberDeclarationSyntax>((NamespaceDeclarationSyntax)currentTree));
+                }
+                currentTree = ns;
+                current = current.Parent;
+            }
+
+            return (MemberDeclarationSyntax)currentTree;
+        }
+    }
 }
